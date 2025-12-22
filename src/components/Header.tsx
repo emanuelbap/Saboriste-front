45e96f37451e065db2e9da5d44cf3803ff4418d2
Emanuel Apolinário <emanuelapolinario99@gmail.com>
2025-12-22 06:02:21 -0300

    integração com o whatsApp feita

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,9 +12,12 @@ export default function Header() {
           </ul>
         </nav>
 
-        <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-full font-medium transition-colors flex items-center gap-2">
+        <a href={`https://wa.me/${55021971378699}?text=${"Olá, gostaria de encomendar... "}`}
+          target="_blank"
+          rel="noopener noreferrer"
+         className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-full font-medium transition-colors flex items-center gap-2">
           📱 WhatsApp
-        </button>
+        </a>
       </div>
     </header>
   )
